d536d9367538ebf80775c37bfc48513b9557c3df
ac-rupesh <157466136+ac-rupesh@users.noreply.github.com>
2025-05-28 09:58:58 +0530

    Update app.py

--- http/app.py
+++ http/app.py
@@ -49,6 +49,12 @@ def go_back(year):
     return 'Welcome to %d!' % (2018 - year)
 
 
+# use int URL converter
+@app.route('/goback/<int:year>')
+def go_back(year):
+    return 'Welcome to %d!' % (2018 - year)
+
+
 # use any URL converter
 @app.route('/colors/<any(blue, white, red):color>')
 def three_colors(color):
